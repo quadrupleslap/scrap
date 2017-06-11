31392d4de6a65e08d812389002757d54a641fef9
Ram <quadrupleslap@gmail.com>
2017-06-11 13:05:39 +1000

    Added Wayland cfg option.

--- build.rs
+++ build.rs
@@ -3,5 +3,6 @@ fn main() {
         println!("cargo:rustc-cfg=quartz");
     } else if cfg!(target_os="linux") {
         println!("cargo:rustc-cfg=x11");
+        println!("cargo:rustc-cfg=wayland");
     }
 }
