d6a36230fb6856138eb39074186c6dfef31cb8d7
myfreeweb <greg@unrelenting.technology>
2017-06-22 00:20:49 +0300

    Use x11 on all unix systems

--- build.rs
+++ build.rs
@@ -1,13 +1,9 @@
 fn main() {
     if cfg!(target_os="macos") {
         println!("cargo:rustc-cfg=quartz");
-    }
-    
-    if cfg!(target_os="linux") {
-        println!("cargo:rustc-cfg=x11");
-    }
-    
-    if cfg!(target_os="windows") {
+    } else if cfg!(target_os="windows") {
         println!("cargo:rustc-cfg=dxgi");
+    } else if cfg!(unix) {
+        println!("cargo:rustc-cfg=x11");
     }
 }
